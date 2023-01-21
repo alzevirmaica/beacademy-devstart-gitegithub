88458f3ae24491f981536e632dcd6468ca084586
Alzevir Maicá <alzevirm@gmail.com>
2023-01-20 23:34:05 -0300

    Tipos de Dados

---
+++ beacademy-empreendev-javascript/scripts.js
@@ -0,0 +1,42 @@
+// Comentário em linha 
+/*
+   Comentário em bloco
+*/
+
+// Strings
+// console.log('Olhá eu no aspas simples!!')
+// console.log(`Olá ai crase `)
+
+// Number
+
+//  console.log(typeof 25) 
+//  console.log(24.5) 
+//  console.log( 12*4/4) 
+//  console.log(Infinity)
+//  console.log(50+(53*4.5))
+//  console.log(Math.round(35.8))
+//  console.log( typeof true)
+//  console.log( 24 > 56)
+
+ //Objeto carro
+//  console.log({
+//     marca: 'Fiat',
+//     largura: '550cm',
+//     peso: 1,
+//     velocidade: (90 * 200) * 2,
+//     andar: function () {
+//         console.log(`sua velocidade é de  200`)
+//     },
+//     possuiAr: true
+//  })
+
+// ARRAy(vetores)
+//  Lista de valores
+//  Agrupamento de dados
+
+console.log([
+   "Bolacha",
+   "Coca-cola",
+   "Verduras",
+   "Agua"
+])
