3a39dbf23f99ea648ed9bc8fcba95e3e3a76e5ef
Alzevir Maicá <alzevirm@gmail.com>
2023-01-28 17:31:20 -0300

    Empty values

---
+++ estudos/JS_HDC/EMPTY VALUES/script.js
@@ -0,0 +1,11 @@
+/*
+    EMPTY VALUES
+
+    * Temos duas palavras reservadas que pertencem a este grupo de dados: Undefined e null;
+    * Undefined geralmente é visto quando utilizamos um código que ainda não definido;
+    * Já null, costuma ser imposto pelos programadores, para determinar que não há ainda um valor;
+*/
+
+console.log(typeof null, typeof undefined)
+console.log(null === undefined)
+console.log(null == undefined)
