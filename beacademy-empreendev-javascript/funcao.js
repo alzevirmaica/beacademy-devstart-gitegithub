ae8b4623743fa1f83ce7658986c2e43a86ff9657
Alzevir Maicá <alzevirm@gmail.com>
2023-02-01 15:26:34 -0300

    Função parte III

--- beacademy-empreendev-javascript/funcao.js
+++ beacademy-empreendev-javascript/funcao.js
@@ -7,50 +7,60 @@
 
 */
 
-// Exemplo 1
-         //Metódo
-function myFristFunction () {
-    //Entra o meu código
-    console.log("Olha a minha função!!")
-}
+// // Exemplo 1
+//          //Metódo
+// function myFristFunction () {
+//     //Entra o meu código
+//     console.log("Olha a minha função!!")
+// }
 
-//Executar
-myFristFunction()
+// //Executar
+// myFristFunction()
 
-// Função com parâmetros e argumentos
-//  parâmetros
-function sum(number1, number2) {
+// // Função com parâmetros e argumentos
+// //  parâmetros
+// function sum(number1, number2) {
     
-    console.log(number1 + number2)
-}
+//     console.log(number1 + number2)
+// }
 
-sum(32, 24) // Argumentos
-
-
-// Função com Return
-function myFirstFunctionWithReturn (num1, num2) {
-    let total = num1 + num2
-    return total
-}
+// sum(32, 24) // Argumentos
 
 
-console.log(myFirstFunctionWithReturn(20, 20))
+// // Função com Return
+// function myFirstFunctionWithReturn (num1, num2) {
+//     let total = num1 + num2
+//     return total
+// }
+
+
+// console.log(myFirstFunctionWithReturn(20, 20))
 
 
 
-// Restaurante
-function meuPratoDeComida(comida1, comida2, comida3, comida4) {
-    return `Meu prato de comida é ${comida1}, ${comida2}, ${comida3}, ${comida4}`
-}
+// // Restaurante
+// function meuPratoDeComida(comida1, comida2, comida3, comida4) {
+//     return `Meu prato de comida é ${comida1}, ${comida2}, ${comida3}, ${comida4}`
+// }
 
-const prato = meuPratoDeComida('arroz', 'feijão', 'bife', 'batata frita')
-console.log(prato)
+// const prato = meuPratoDeComida('arroz', 'feijão', 'bife', 'batata frita')
+// console.log(prato)
 
-//Arrow Function
-const myFirstArrowFunction = (n1, n2) => {
-    let total = n1 + n2
-    return total
-}
+// //Arrow Function
+// const myFirstArrowFunction = (n1, n2) => {
+//     let total = n1 + n2
+//     return total
+// }
 
-const result = myFirstArrowFunction(20,20)
-console.log(result)
+// const result = myFirstArrowFunction(20,20)
+// console.log(result)
+
+//Função anônima
+
+(function () {
+    console.log("Paylivre s2")
+}) () //Autoexecução
+
+setTimeout(function() { // atrasa o tempo para ser excecutado
+    console.log("Vou levar 3 segundos para ser executado!")
+},3000)//3s
