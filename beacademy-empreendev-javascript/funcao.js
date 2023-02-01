6205e61497d262f57160c7234df19780d7706755
Alzevir Maicá <alzevirm@gmail.com>
2023-01-31 21:56:03 -0300

    Função parte II

--- beacademy-empreendev-javascript/funcao.js
+++ beacademy-empreendev-javascript/funcao.js
@@ -35,3 +35,22 @@ function myFirstFunctionWithReturn (num1, num2) {
 
 
 console.log(myFirstFunctionWithReturn(20, 20))
+
+
+
+// Restaurante
+function meuPratoDeComida(comida1, comida2, comida3, comida4) {
+    return `Meu prato de comida é ${comida1}, ${comida2}, ${comida3}, ${comida4}`
+}
+
+const prato = meuPratoDeComida('arroz', 'feijão', 'bife', 'batata frita')
+console.log(prato)
+
+//Arrow Function
+const myFirstArrowFunction = (n1, n2) => {
+    let total = n1 + n2
+    return total
+}
+
+const result = myFirstArrowFunction(20,20)
+console.log(result)
