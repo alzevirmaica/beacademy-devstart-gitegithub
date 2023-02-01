41ce3bc6ddd3f4a4a2b53b49453a5db5f1abfd67
Alzevir Maicá <alzevirm@gmail.com>
2023-02-01 16:45:43 -0300

    Funções parte V

--- beacademy-empreendev-javascript/funcao.js
+++ beacademy-empreendev-javascript/funcao.js
@@ -99,12 +99,65 @@
  //parte 4
 
  
- let assunto = 'Estudar' // var = valor
+//  let assunto = 'Estudar' // var = valor
 
- function criarAssunto() { //Sem parâmetro
-     assunto = "Escutar música" // Mudança de valor
-     return assunto //
- }
- console.log(assunto) //resposta => 'Estudar
- console.log(criarAssunto(assunto)) // resposta=> 'Escutar música'
+//  function criarAssunto() { //Sem parâmetro
+//      assunto = "Escutar música" // Mudança de valor
+//      return assunto //
+//  }
+//  console.log(assunto) //resposta => 'Estudar
+//  console.log(criarAssunto(assunto)) // resposta=> 'Escutar música'
 
+// Hoisting
+
+// digaMinhaIdade()
+
+// function digaMinhaIdade() {
+//     return console.log(30)
+// }
+
+// callback
+
+// function minhaFuncao (callback) {
+//     console.log('Inicio da minha função =)')
+
+//     callback(15, 15)
+
+//     console.log('Final da minha função')
+// }
+
+// minhaFuncao(
+//     (num1, num2) => {
+//         return console.log(num1 + num2)
+//     }
+// )
+
+// function construct
+
+function Sum () {
+    this.num1,
+    this.num2,
+    this.calc = function (num1, num2) {
+        return total = num1 + num2
+    }
+}
+
+// //
+// const math = new Sum()
+// const num1 = math.num1 = 24
+// const num2 = math.num2 = 50
+
+// console.log(num1)
+// console.log(num2)
+// console.log(math.calc(num1,num2))
+
+// const math2 = new Sum()
+// const number1 = math.num1 = 30
+// const number2 = math.num2 = 10
+// console.log(math.calc(number1, number2))
+
+const math = new Sum().calc(45,45)
+console.log(math)
+
+const data = new Date().getDay()
+console.log(data)
