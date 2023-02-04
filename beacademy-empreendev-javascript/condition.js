79e2c4bb6acec6ba047797e7f16fc83e52db3da3
Alzevir Maicá <alzevirm@gmail.com>
2023-02-04 02:18:49 -0300

    Controle e condições (Throw e Try and Catch)

--- beacademy-empreendev-javascript/condition.js
+++ beacademy-empreendev-javascript/condition.js
@@ -1,58 +1,77 @@
 // if and else
 // se e se não
 
-let num = 10
-let num2 = 20
-let isAdmin = true
+// let num = 10
+// let num2 = 20
+// let isAdmin = true
 
-if(num >= 10) {
-    console.log('Sou maior')
-}else if(num >= 10 && num2 <= 20) {
-    console.log('Segunda resposta verdade')
-}else {
-    console.log('Resposta falsa')
-}
+// if(num >= 10) {
+//     console.log('Sou maior')
+// }else if(num >= 10 && num2 <= 20) {
+//     console.log('Segunda resposta verdade')
+// }else {
+//     console.log('Resposta falsa')
+// }
 
 //Switch
 
-let myExpression = 'd'
+// let myExpression = 'd'
 
-switch(myExpression) {
-    case 'a':
-        console.log("Apertou o A")
-        break
-    case 'b':
-        console.log("Apertou o B")
-        break
-    case 'c':
-        console.log("Apertou o c")
-        break
-    default:
-        console.log("Você apertou outra tecla", myExpression)
-        break
-}
+// switch(myExpression) {
+//     case 'a':
+//         console.log("Apertou o A")
+//         break
+//     case 'b':
+//         console.log("Apertou o B")
+//         break
+//     case 'c':
+//         console.log("Apertou o c")
+//         break
+//     default:
+//         console.log("Você apertou outra tecla", myExpression)
+//         break
+// }
 
 //Calculadora
-function calc (num1,operador,num2){
-    let result
+// function calc (num1,operador,num2){
+//     let result
 
-    switch(operador) {
-        case "+":
-            result = num1 + num2
-            break  
-        case "-":
-            result = num1 - num2
-            break
-        case "*":
-            result = num1 * num2
-            break
-        case "/":
-            result = num1 / num2
-            break
-        default:
-            console.log("Não entendi")
-            break
+//     switch(operador) {
+//         case "+":
+//             result = num1 + num2
+//             break  
+//         case "-":
+//             result = num1 - num2
+//             break
+//         case "*":
+//             result = num1 * num2
+//             break
+//         case "/":
+//             result = num1 / num2
+//             break
+//         default:
+//             console.log("Não entendi")
+//             break
+//     }
+//     return result
+// }
+// console.log(calc(8, "/", 2))
+
+
+//Throw and try catch
+
+//Saber a minha idade
+function myAge(myage) {
+    if(!myage) {
+        throw "Você precisa colocar a idade"
     }
-    return result
+    console.log("Eu executei depois do erro!")
 }
-console.log(calc(8, "/", 2))
+try {
+    myAge(30)
+    console.log("Já tratei e deu certo")
+} catch(error) {
+    console.log(error)
+}
+
+console.log('Programa continua')
