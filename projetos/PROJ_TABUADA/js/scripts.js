0dcb9b39fe4d8d7b5930e5cadf7975168b5c4c77
Alzevir Maicá <alzevirm@gmail.com>
2023-02-12 23:39:34 -0300

    Projeto tabuada-html e css do projeto

---
+++ projetos/PROJ_TABUADA/js/scripts.js
@@ -0,0 +1,12 @@
+//Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+
+//funções
+
+//Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+})
