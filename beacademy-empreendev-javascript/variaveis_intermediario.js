309b951aaf668c47f4ee8481eaa086a93d041412
Alzevir Maicá <alzevirm@gmail.com>
2023-02-14 23:19:29 -0300

    Variáveis-JavaScript Intermediário

---
+++ beacademy-empreendev-javascript/variaveis_intermediario.js
@@ -0,0 +1,26 @@
+/*
+  --VARIÁVEIS
+
+  -Variáveis definidas com let não podem ser redeclaradas; 
+  - Variáveis definidas com let devem ser declaradas antes do uso;
+  - Variáveis definidas com let têm block scope.
+*/
+
+
+// Scope
+{
+    let x = 2
+    console.log(x)
+}
+
+{
+    let y = 2
+}
+
+// console.log(y)
+
+//Reassing a value 
+let w = 10
+w = 5
+console.log(w)
+
