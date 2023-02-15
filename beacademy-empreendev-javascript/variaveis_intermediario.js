43bdede150278075a1ac441b57a078d59998873d
Alzevir Maicá <alzevirm@gmail.com>
2023-02-15 00:02:06 -0300

    IIFE e Arrow Functions

--- beacademy-empreendev-javascript/variaveis_intermediario.js
+++ beacademy-empreendev-javascript/variaveis_intermediario.js
@@ -24,3 +24,16 @@ let w = 10
 w = 5
 console.log(w)
 
+
+// Templeate Strings
+
+let firstName = "José"
+let lastName = "Da Silva"
+
+let text = `Welcome ${firstName}, ${lastName}`
+console.log(text)
+
+let price = 10;
+let fees = 0.25
+let total = `O total do produto é: R$ ${price + fees}`
+ console.log(total)
